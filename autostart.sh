2484b22452961705ac7c304f76c4bc37307c5bc5
TwistyGt <braydenchuon@gmail.com>
2026-09-13 21:01:11 -0700

    Add dbus-update-activation-environment command

--- autostart.sh
+++ autostart.sh
@@ -1,2 +1,7 @@
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=MangoWC
+
 #!/bin/sh
 quickshell -p $HOME/.config/noctalia &
+
+# polkit-gnome:
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
